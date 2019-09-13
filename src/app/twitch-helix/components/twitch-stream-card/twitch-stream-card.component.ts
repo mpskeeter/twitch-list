a714afe734daf2f014d83cf7956578c243e2fae7
Mark Peters <mpskeeter@gmail.com>
2019-09-13 16:01:39 -0400

    Added isSubscribed.

--- src/app/twitch-helix/components/twitch-stream-card/twitch-stream-card.component.ts
+++ src/app/twitch-helix/components/twitch-stream-card/twitch-stream-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { TwitchUser, TwitchClip } from '../../models';
+import { TwitchUser, TwitchClip, TwitchSubscriptionSuccess } from '../../models';
 
 @Component({
   selector: 'twitch-stream-card',
@@ -15,6 +15,10 @@ export class TwitchStreamCardComponent {
   constructor(
     public router: Router,
   ) { }
+
+  isSubscriptionSuccess = (object: any): object is TwitchSubscriptionSuccess => '_id' in object;
+
+  isSubscribed = () => this.isSubscriptionSuccess(this.user.subscription);
 
   viewStream = (userName: string) => {
     this.router.navigate(['/twitch', 'viewstream', userName]);
